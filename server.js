54e6d3b40b11fa874982c20d3dcf68a5c4975353
Pravin kanna P <mp.pravinkanna@gmail.com>
2020-08-16 09:33:14 +0530

    changed port to 3002

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-const PORT = 3001 || process.env.PORT;
+const PORT = 3002 || process.env.PORT;
 
 const { runCode } = require('./server/controllers/runcode')
 
