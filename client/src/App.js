4eaa3b82d36db96cab833dd986e279be6138d488
Pravin kanna P <mp.pravinkanna@gmail.com>
2020-08-30 15:16:50 +0530

    added morgan & configured prod and dev configs

--- client/src/App.js
+++ client/src/App.js
@@ -16,7 +16,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <Ide></Ide>
+        <Ide />
       </div>
     );
   }
--- server.js
+++ server.js
@@ -3,33 +3,39 @@ const app = express();
 const dotenv = require('dotenv');
 const path = require('path');
 const morgan = require('morgan');
-
-
-//configs
 const connectDB = require('./config/db')
+
+//Routes path config
+const ide = require('./routes/ide');
+
+//Load configs
 dotenv.config({ path: './config/config.env' })
 
 //Connect to Database
 connectDB();
 
-//Express bodyparser
+//Morgan middleware
+if (process.env.NODE_ENV === 'development') {
+    app.use(morgan('dev'));
+}
+
+//Express bodyparser middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 
-//Router definition
-const ide = require('./routes/ide');
-
 //Routes
 app.use('/api/ide', ide);
 
-// To use Production Build
-app.use(express.static('client/build'));
+if (process.env.NODE_ENV === 'production') {
+    // To use Production Build
+    app.use(express.static('client/build'));
 
-app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-});
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+    });
+}
 
 // Server
 const PORT = process.env.PORT || 3002;
-app.listen(PORT, console.log("Server started on PORT:", PORT))
+app.listen(PORT, console.log(`Server started \t Mode: ${process.env.NODE_ENV} PORT: ${PORT}`))
