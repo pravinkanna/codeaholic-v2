42e608fb690dede5304a0886f4437d249ae6d118
Pravin kanna P <mp.pravinkanna@gmail.com>
2020-09-21 10:01:40 +0530

    moved Ide.js to App.js

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,11 +1,20 @@
-import React from "react";
-import { PageView, initGA } from '../tracking';
-// import { BrowserRouter as Router, Route, Link } from "react-router-dom";
-import { AuthProvider } from '../contexts/AuthContext';
-import { IdeProvider } from '../contexts/IdeContext';
-import Ide from "./Ide";
-import "bootstrap/dist/css/bootstrap.min.css";
+import React, { useEffect, useContext, useState } from "react";
+import SplitPane from "react-split-pane";
+
+import IdeNavbar from "./Navbar";
+import IdeEditor from "./Editor";
+import IdeInput from "./Input";
+import IdeOutput from "./Output";
+import IdeShareModal from "./ShareModal";
+import IdeLoginModal from "./LoginModal";
 import "./App.css";
+
+import { PageView, initGA } from '../tracking';
+
+
+
+import { IdeContext } from "../contexts/IdeContext";
+import { getShare } from "../apis/share";
 
 
 function App() {
@@ -13,15 +22,58 @@ function App() {
   initGA('UA-175900721-1');
   PageView();
 
+  const [shareId, setShareId] = useState(null);
+  const [resizeEditor, setResizeEditor] = useState(false);
+  const { setCode, setLanguageId, width, setWidth } = useContext(IdeContext);
+
+  const handleChange = () => setResizeEditor((prev) => !prev);
+
+  useEffect(() => {
+    // Handler to call on window resize
+    const handleResize = () => setWidth(window.innerWidth);
+
+    // Add event listener
+    window.addEventListener("resize", handleResize);
+
+    // Call handler right away so state gets updated with initial window size
+    handleResize();
+
+    // Remove event listener on cleanup
+    return () => window.removeEventListener("resize", handleResize);
+  }, [width, setWidth]);
+
+  useEffect(() => {
+    //Function to get code from database and set to context
+    const getSharedCode = async (shareId) => {
+      const result = await getShare(shareId);
+      setCode(result.data.data.source_code);
+      setLanguageId(result.data.data.language_id);
+    };
+
+    //Extracting shareID from URL
+    let search = window.location.search;
+    let params = new URLSearchParams(search);
+    setShareId(params.get("shareId"));
+    if (shareId) {
+      getSharedCode(shareId);
+    }
+  }, [shareId, setCode, setLanguageId]);
 
 
   return (
     <div className="App">
-      <AuthProvider>
-        <IdeProvider>
-          <Ide />
-        </IdeProvider>
-      </AuthProvider>
+      <SplitPane split="horizontal" allowResize={false}>
+        <IdeNavbar />
+        <SplitPane split={width >= 767 ? "vertical" : "horizontal"} minSize={0} maxSize={-5} defaultSize="60%" onChange={handleChange}>
+          <IdeEditor resizeEditor={resizeEditor} setResizeEditor={setResizeEditor} />
+          <SplitPane split="horizontal" allowResize={false} defaultSize="50%">
+            <IdeInput />
+            <IdeOutput />
+          </SplitPane>
+        </SplitPane>
+      </SplitPane>
+      <IdeShareModal />
+      <IdeLoginModal />
     </div>
   );
 }
--- client/src/index.js
+++ client/src/index.js
@@ -1,14 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import App from './components/App';
+
+
+import { AuthProvider } from './contexts/AuthContext';
+import { IdeProvider } from './contexts/IdeContext';
+
 import "bootstrap/dist/css/bootstrap.min.css";
 import './index.css';
-import App from './components/App';
+
 
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <AuthProvider>
+      <IdeProvider>
+        <App />
+      </IdeProvider>
+    </AuthProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
