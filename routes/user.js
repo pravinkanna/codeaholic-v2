98be7b52abc9bb4379e1c5315e34c950af694995
Pravin kanna P <mp.pravinkanna@gmail.com>
2023-06-11 14:07:10 +0530

    fixed untracked files

--- routes/user.js
+++ routes/user.js
@@ -6,20 +6,20 @@ const passport = require('passport');
 
 
 router.route('/register')
-    .post(register)
+  .post(register)
 
 router.route('/login')
-    .post(passport.authenticate('local', { session: false }), login)
+  .post(passport.authenticate('local', { session: false }), login)
 
 router.route('/logout')
-    .get(passport.authenticate('jwt', { session: false }), logout)
+  .get(passport.authenticate('jwt', { session: false }), logout)
 
 router.route('/save')
-    .get(passport.authenticate('jwt', { session: false }), getSaveProgram)
-    .post(passport.authenticate('jwt', { session: false }), saveProgram)
+  .get(passport.authenticate('jwt', { session: false }), getSaveProgram)
+  .post(passport.authenticate('jwt', { session: false }), saveProgram)
 
 router.route('/authenticated')
-    .get(passport.authenticate('jwt', { session: false }), authenticated)
+  .get(passport.authenticate('jwt', { session: false }), authenticated)
 
 
 module.exports = router;
